76df8b0c3fad039ee24bb3d44fc0701bd3ad0d22
Lolly Dormido <lols@99centbrains.com>
2014-04-16 18:35:08 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yoshirt::Application.config.secret_key_base = '11268d3a1405b4bdd1b058d6e6664d77cb4d3b8620cf427ff96b2cbcb424816045887c54b4f3e4360cdcfb9d082e8d91ac9931e7e4e81f329214a3d0aac1da81'
