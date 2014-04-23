1143114d42caf7e9416fa6067d5f39779d8ce894
Lolly Dormido <lols@99centbrains.com>
2014-04-22 21:47:12 -0700

    adjusted production.rb with stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,19 @@ Yoshirt::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Use for devise mailer
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'http://peaceful-atoll-4795.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp 
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
+
+  config.action_mailer.raise_delivery_errors = true
   
 end
